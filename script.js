5aa096ba40b6e52597067682b30f08466dca3d4f
SKY6238483 <sukanya.b6238483@gmail.com>
2026-08-29 09:09:33 +0700

    add

--- script.js
+++ script.js
@@ -1131,7 +1131,7 @@ function renderProductDetail() {
 
                 <div class="product-detail-price">
 
-                    ${product.price.toLocaleString()} / ${product.quantity}
+                    ${product.price.toLocaleString()} / ${product.unit}
 
                     /ตร.ม.
 
